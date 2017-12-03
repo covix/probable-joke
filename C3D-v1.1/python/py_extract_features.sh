0b2192e2ba0f2bc21b94cc7b8b86433cc3e13ae3
schenock <mitrevdane@gmail.com>
2017-12-03 21:34:00 +0100

    Added python scripts for extracting features.

---
+++ C3D-v1.1/python/py_extract_features.sh
@@ -0,0 +1,2 @@
+python2 extract_features.py '/home/schenock/caffe/data/dane_test_asl' '/home/schenock/caffe/models/bvlc_alexnet/bvlc_alexnet.caffemodel' '/home/schenock/caffe/models/bvlc_alexnet/deploy.prototxt' '/home/schenock/caffe/data/ilsvrc12/ilsvrc_2012_mean.npy' '/home/schenock/caffe/examples/_pyfeatures_test' 'fc7'
+
