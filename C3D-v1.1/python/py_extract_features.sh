b78146e8034716deccb1339cb39d53cfb3eb41eb
schenock <mitrevdane@gmail.com>
2017-12-08 18:43:11 +0100

    Fun fun fun and some PCA

--- C3D-v1.1/python/py_extract_features.sh
+++ C3D-v1.1/python/py_extract_features.sh
@@ -1,2 +1,2 @@
-python2 extract_features.py '/home/schenock/caffe/data/dane_test_asl' '/home/schenock/caffe/models/bvlc_alexnet/bvlc_alexnet.caffemodel' '/home/schenock/caffe/models/bvlc_alexnet/deploy.prototxt' '/home/schenock/caffe/data/ilsvrc12/ilsvrc_2012_mean.npy' '/home/schenock/caffe/examples/_pyfeatures_test' 'fc7'
+python2 extract_features.py '/home/schenock/caffe/data/_test_vid1' '/home/schenock/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel' '/home/schenock/caffe/models/bvlc_googlenet/deploy.prototxt' '/home/schenock/caffe/data/ilsvrc12/ilsvrc_2012_mean.npy' '/home/schenock/caffe/examples/_pyfeatures_test' 'pool5/7x7_s1'
 
