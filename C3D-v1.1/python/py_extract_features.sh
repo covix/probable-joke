505105a39ca5e165e2b871a7d42512ba1d97b1d2
schenock <mitrevdane@gmail.com>
2017-12-09 18:51:52 +0100

    Created labels file. Scripts reorganization

--- C3D-v1.1/python/py_extract_features.sh
+++ C3D-v1.1/python/py_extract_features.sh
@@ -1,2 +1,3 @@
-python2 extract_features.py '/home/schenock/caffe/data/_test_vid1' '/home/schenock/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel' '/home/schenock/caffe/models/bvlc_googlenet/deploy.prototxt' '/home/schenock/caffe/data/ilsvrc12/ilsvrc_2012_mean.npy' '/home/schenock/caffe/examples/_pyfeatures_test' 'pool5/7x7_s1'
+
+python2 extract_features_pca.py '/home/schenock/caffe/data/_test_vid1' '/home/schenock/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel' '/home/schenock/caffe/models/bvlc_googlenet/deploy.prototxt' '/home/schenock/caffe/data/ilsvrc12/ilsvrc_2012_mean.npy' '/home/schenock/caffe/examples/_pyfeatures_test/features' 'pool5/7x7_s1'
 
