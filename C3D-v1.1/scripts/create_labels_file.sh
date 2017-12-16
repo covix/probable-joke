938cfc2e2664ef5d36dcc12f14db2ae115829a92
Luca Coviello <luca.coviello@gmail.com>
2017-12-17 00:48:26 +0100

    create_labels_file.sh

--- C3D-v1.1/scripts/create_labels_file.sh
+++ C3D-v1.1/scripts/create_labels_file.sh
@@ -4,6 +4,5 @@ for i in `ls $DATAFOLDER`
 do
 	# Get the 3rd component of the name that is the class label
 	CLASS_LABEL=`basename $i | cut -d- -f 3`
-	echo $i $CLASS_LABEL
+	echo ${DATA_FOLDER}/$i $CLASS_LABEL
 done
-
