e719ad851ec41f174d6994a14e675b4f2b6fad1f
Luca Coviello <luca.coviello@gmail.com>
2018-01-09 22:19:22 +0100

    whopsy

--- C3D-v1.1/examples/asl/run_finetune_asl_c3d_4gpus.sh
+++ C3D-v1.1/examples/asl/run_finetune_asl_c3d_4gpus.sh
@@ -1,6 +1,6 @@
 SOLVER_FILE=$1
-oarsub -p "gpu='YES'" -l /nodes=1/gpunum=4,walltime=12 -S ./finetuning_asl_4gpus.sh $SOLVER_FILE
+oarsub -p "gpu='YES'" -l /nodes=1/gpunum=4,walltime=12 -S "./finetuning_asl_4gpus.sh $SOLVER_FILE"
 
-# oarsub -p "gpu='YES'" -l /nodes=1/gpunum=4,walltime=12 -S ./finetuning_asl_4gpus.sh solver_r2_asl.prototxt
+# oarsub -p "gpu='YES'" -l /nodes=1/gpunum=4,walltime=12 -S "./finetuning_asl_4gpus.sh solver_r2_asl.prototxt"
 
 # oarsub -I -p "gpu='YES'" -l /gpunum=1
