dde593d10ed8ef72e18751c40abcee4da90b2e4c
Luca Coviello <luca.coviello@gmail.com>
2018-01-11 00:01:11 +0100

    training on pimw

--- C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
+++ C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
@@ -1,4 +1,6 @@
-oarsub -p "gpu='YES'" -l walltime=12 -S ./finetuning_asl.sh 
+SOLVER_FILE=$1
+
+oarsub -p "gpu='YES'" -l walltime=12 -S "./finetuning_asl.sh SOLVER_FILE=$1"
 
 
 # oarsub -I -p "gpu='YES'" -l /gpunum=1
