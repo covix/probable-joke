f21185a5649eba4c1b21dc26368600fd5c8c0888
Luca Coviello <luca.coviello@gmail.com>
2018-01-10 23:58:34 +0100

    training on pimw

--- C3D-v1.1/examples/asl/finetuning_asl.sh
+++ C3D-v1.1/examples/asl/finetuning_asl.sh
@@ -5,11 +5,11 @@ fi
 mkdir -p LOG_TRAIN
 
 source /etc/profile.d/modules.sh
-module load cuda/7.5
+module load cuda/8.0
 module load cudnn/5.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
 
 CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
 
-GLOG_log_dir="./LOG_TRAIN"  $CAFFE_HOME/build/tools/caffe.bin train --solver=solver_r2_asl.prototxt --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0,1,2,3
+GLOG_log_dir="./LOG_TRAIN"  $CAFFE_HOME/build/tools/caffe.bin train --solver=solver_r2_asl.prototxt --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0
--- C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
+++ C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
@@ -1,5 +1,4 @@
-oarsub -p "gpu='YES'" -l /gpunum=4,walltime=12 -S ./finetuning_asl.sh 
+oarsub -p "gpu='YES'" -l walltime=12 -S ./finetuning_asl.sh 
 
 
 # oarsub -I -p "gpu='YES'" -l /gpunum=1
-
