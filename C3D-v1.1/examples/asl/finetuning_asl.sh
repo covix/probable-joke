b90a8e1126a2338fc96700a29fa9d73eef6e3a2a
Luca Coviello <luca.coviello@gmail.com>
2018-01-10 22:35:07 +0100

    updated finetuning script

--- C3D-v1.1/examples/asl/finetuning_asl.sh
+++ C3D-v1.1/examples/asl/finetuning_asl.sh
@@ -8,4 +8,4 @@ source /etc/profile.d/modules.sh
 module load cuda/7.5
 module load cudnn/5.0
 
-GLOG_log_dir="./LOG_TRAIN" ../../build/tools/caffe.bin train --solver=solver_r2_asl.prototxt --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0,1,2,3
+GLOG_log_dir="./LOG_TRAIN"  $CAFFE_HOME/build/tools/caffe.bin train --solver=solver_r2_asl.prototxt --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0,1,2,3
