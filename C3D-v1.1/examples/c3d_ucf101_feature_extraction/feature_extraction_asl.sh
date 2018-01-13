7e062ca7ea4fd8b81d760671158223eb01f6ba3d
Emily Soehler <emily@ginger.local>
2018-01-13 16:37:20 +0100

    Testing feature extraction

--- C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction_asl.sh
+++ C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction_asl.sh
@@ -2,6 +2,10 @@ if [ ! -f c3d_resnet18_sports1m_r2_iter_2800000.caffemodel ];then
 	wget https://www.dropbox.com/s/qqfrg6h44d4jb46/c3d_resnet18_sports1m_r2_iter_2800000.caffemodel
 fi
 
+GPU_ID=0
+BATCH_SIZE=10
+BATCH_NUMBERS=1 
+
 source /etc/profile.d/modules.sh
 module load cuda/8.0
 module load cudnn/5.0
@@ -10,4 +14,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
 
 CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
 
-GLOG_logtostderr=1 ${CAFFE_HOME}/build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel 0 30 4970 ucf101_video_frame.prefix prob pool5 res5b
+GLOG_logtostderr=1 ${CAFFE_HOME}/build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel $GPU_ID $BATCH_SIZE $BATCH_NUMBERS ucf101_video_frame.prefix pool5 res5b
