cf1c32d5d2d32d167f5079ee86ad01459fc3b72f
Luca Coviello <luca.coviello@gmail.com>
2018-01-13 21:09:36 +0100

    added low fps network

--- C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
+++ C3D-v1.1/examples/asl/run_finetune_asl_c3d.sh
@@ -1,6 +1,5 @@
 SOLVER_FILE=$1
-
-oarsub -p "gpu='YES'" -l walltime=12 -S "./finetuning_asl.sh SOLVER_FILE=$1"
+oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" -l /gpunum=1,walltime=12 -t besteffort -S "./finetuning_asl.sh $SOLVER_FILE"
 
 
 # oarsub -I -p "gpu='YES'" -l /gpunum=1
