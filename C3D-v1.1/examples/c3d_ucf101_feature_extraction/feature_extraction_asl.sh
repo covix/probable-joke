b961f3eefa393ef84dde2921ea99e353d74d77c0
Luca Coviello <luca.coviello@gmail.com>
2018-01-13 16:01:28 +0100

    added feature extraction file for asl

---
+++ C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction_asl.sh
@@ -0,0 +1,13 @@
+if [ ! -f c3d_resnet18_sports1m_r2_iter_2800000.caffemodel ];then
+	wget https://www.dropbox.com/s/qqfrg6h44d4jb46/c3d_resnet18_sports1m_r2_iter_2800000.caffemodel
+fi
+
+source /etc/profile.d/modules.sh
+module load cuda/8.0
+module load cudnn/5.0
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
+
+CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
+
+GLOG_logtostderr=1 ${CAFFE_HOME}/build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel 0 30 4970 ucf101_video_frame.prefix prob pool5 res5b
