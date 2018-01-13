7dfaf91a2098808c836ee97ec5006ababd59bc6c
Luca Coviello <luca.coviello@gmail.com>
2018-01-13 16:57:30 +0100

    trying different weights

--- C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction_asl.sh
+++ C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction_asl.sh
@@ -10,4 +10,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
 
 CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
 
-GLOG_logtostderr=1 ${CAFFE_HOME}/build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel 0 30 4970 ucf101_video_frame.prefix prob pool5 res5b
+GLOG_logtostderr=1 ${CAFFE_HOME}/build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt conv3d_deepnetA_sport1m_iter_1900000_v1.1.caffemodel 0 30 4970 ucf101_video_frame.prefix prob pool5 res5b
