849a7820d8da84c9460b99397b2607c92731f041
Luca Coviello <luca.coviello@gmail.com>
2018-01-14 15:12:13 +0100

    new caffe

--- C3D-v1.1/examples/asl/finetuning_asl.sh
+++ C3D-v1.1/examples/asl/finetuning_asl.sh
@@ -8,10 +8,12 @@ mkdir -p LOG_TRAIN
 
 source /etc/profile.d/modules.sh
 module load cuda/8.0
+module load cuda/7.5
 module load cudnn/5.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
 
-CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
+# CAFFE_HOME=/data/sparks/share/C3D/C3D-v1.1/
+CAFFE_HOME=/data/sparks/share/video-caffe/
 
 GLOG_log_dir="./LOG_TRAIN" $CAFFE_HOME/build/tools/caffe.bin train --solver=${SOLVER_FILE} --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0
