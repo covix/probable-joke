4096cb16b68572ffb4e288bfe4e456e7d247c1e0
Luca Coviello <luca.coviello@gmail.com>
2018-01-14 14:24:26 +0100

    parsing 3 networks

--- C3D-v1.1/tools/extra/parse_log.sh
+++ C3D-v1.1/tools/extra/parse_log.sh
@@ -10,36 +10,57 @@ DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ "$#" -lt 1 ]
 then
-echo "Usage parse_log.sh /path/to/your.log"
-exit
+    echo "Usage parse_log.sh /path/to/your.log"
+    exit
 fi
 LOG=`basename $1`
 sed -n '/Iteration .* Testing net/,/Iteration *. loss/p' $1 > aux.txt
-sed -i '/Waiting for data/d' aux.txt
-sed -i '/prefetch queue empty/d' aux.txt
-sed -i '/Iteration .* loss/d' aux.txt
-sed -i '/Iteration .* lr/d' aux.txt
-sed -i '/Train net/d' aux.txt
-grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
-grep 'Test net output #0' aux.txt | awk '{print $11}' > aux1.txt
-grep 'Test net output #1' aux.txt | awk '{print $11}' > aux2.txt
+sed -i.bak '/Waiting for data/d' aux.txt
+sed -i.bak '/prefetch queue empty/d' aux.txt
+sed -i.bak '/Iteration .* loss/d' aux.txt
+sed -i.bak '/Iteration .* lr/d' aux.txt
+sed -i.bak '/Train net/d' aux.txt
+# FIXME extracting 2 times same iteration
+grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' | uniq > aux0.txt
+
+# Test on train
+grep -A3 'Testing net (#0)' aux.txt > aux_train.txt
+grep 'Test net output #0' aux_train.txt | awk '{print $11}' > aux_train_acc1.txt
+grep 'Test net output #1' aux_train.txt | awk '{print $11}' > aux_train_acc5.txt
+grep 'Test net output #2' aux_train.txt | awk '{print $11}' > aux_train_loss.txt
+
+# Test on test
+grep -A3 'Testing net (#1)' aux.txt > aux_test.txt
+grep 'Test net output #0' aux_test.txt | awk '{print $11}' > aux_test_acc1.txt
+grep 'Test net output #1' aux_test.txt | awk '{print $11}' > aux_test_acc5.txt
+grep 'Test net output #2' aux_test.txt | awk '{print $11}' > aux_test_loss.txt
 
 # Extracting elapsed seconds
 # For extraction of time since this line contains the start time
-grep '] Solving ' $1 > aux3.txt
-grep 'Testing net' $1 >> aux3.txt
-$DIR/extract_seconds.py aux3.txt aux4.txt
+grep '] Solving ' $1 > aux_seconds_train_raw.txt
+grep 'Testing net (#0)' $1 >> aux_seconds_train_raw.txt
+$DIR/extract_seconds.py aux_seconds_train_raw.txt aux_seconds_train.txt
 
-# Generating
-echo '#Iters Seconds TestAccuracy TestLoss'> $LOG.test
-paste aux0.txt aux4.txt aux1.txt aux2.txt | column -t >> $LOG.test
-rm aux.txt aux0.txt aux1.txt aux2.txt aux3.txt aux4.txt
+grep '] Solving ' $1 > aux_seconds_test_raw.txt
+grep 'Testing net (#1)' $1 >> aux_seconds_test_raw.txt
+$DIR/extract_seconds.py aux_seconds_test_raw.txt aux_seconds_test.txt
+
+# Generating train acc
+echo '#Iters Seconds TestAccuracy1 TestAccuracy5 TestLoss'> $LOG.test_on_train
+paste aux0.txt aux_seconds_train.txt aux_train_acc1.txt aux_train_acc5.txt aux_train_loss.txt | column -t >> $LOG.test_on_train
+rm aux_train.txt aux_train_acc1.txt aux_train_acc5.txt aux_train_loss.txt aux_seconds_train_raw.txt aux_seconds_train.txt
+
+# Generating test acc
+echo '#Iters Seconds TestAccuracy1 TestAccuracy5 TestLoss'> $LOG.test_on_test
+paste aux0.txt aux_seconds_test.txt aux_test_acc1.txt aux_test_acc5.txt aux_test_loss.txt | column -t >> $LOG.test_on_test
+rm aux.txt aux_test.txt aux0.txt aux_test_acc1.txt aux_test_acc5.txt aux_test_loss.txt aux_seconds_test_raw.txt aux_seconds_test.txt aux.txt.bak
+
 
 # For extraction of time since this line contains the start time
 grep '] Solving ' $1 > aux.txt
 grep ', loss = ' $1 >> aux.txt
 grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
-grep ', loss = ' $1 | awk '{print $9}' > aux1.txt
+grep ', loss = ' $1 | awk '{print $13}' > aux1.txt
 grep ', lr = ' $1 | awk '{print $9}' > aux2.txt
 
 # Extracting elapsed seconds
