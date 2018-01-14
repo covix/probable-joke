82640825dc3667034c746a6619d6e67bb7c5a23d
Luca Coviello <luca.coviello@gmail.com>
2018-01-14 17:47:53 +0100

    Added split for R-C3D

--- C3D-v1.1/python/split_train_test_RC3D.py
+++ C3D-v1.1/python/split_train_test_RC3D.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    if sys.argv != 6:
+    if len(sys.argv) != 6:
         print "Usage: python {} input_file, test_size, block_size, train_out_file, test_out_file".format(os.path.basename(__file__))
         sys.exit(-1)
 
@@ -16,6 +16,7 @@ def main():
     test_out_file = sys.argv[5]
 
     times = int(180 / block_size) - 1
+    stride = 1
 
     if((180 / float(block_size)) - (times + 1) != 0):
         print("Attention, block_size is not a multiple of 180 \nin this way you will ignore some frames!")
@@ -25,7 +26,7 @@ def main():
     Y = []
     for f in X:
         for i in range(times):
-            Y.append(f + " " + str(i * block_size + 1))
+            Y.append(f + " " + str(i * block_size + 1) + " " + stride)
     Y = map(lambda x: x + " " + x.split("-")[2], Y)
     X_train, X_test = train_test_split(Y, test_size=test_size)
 
