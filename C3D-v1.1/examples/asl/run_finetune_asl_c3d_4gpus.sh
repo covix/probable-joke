d6a7793021bb15948a0e370f9aedc20d7812b6e4
Luca Coviello <luca.coviello@gmail.com>
2018-01-14 18:32:45 +0100

    Added split for R-C3D

--- C3D-v1.1/examples/asl/run_finetune_asl_c3d_4gpus.sh
+++ C3D-v1.1/examples/asl/run_finetune_asl_c3d_4gpus.sh
@@ -1,5 +1,5 @@
 SOLVER_FILE=$1
-oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" -l /nodes=1/gpunum=4,walltime=12 -t besteffor -S "./finetuning_asl_4gpus.sh $SOLVER_FILE"
+oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" -l /nodes=1/gpunum=4,walltime=12 -t besteffort -S "./finetuning_asl_4gpus.sh $SOLVER_FILE"
 
 # oarsub -p "gpu='YES'" -l /nodes=1/gpunum=4,walltime=12 -S "./finetuning_asl_4gpus.sh solver_r2_asl.prototxt"
 
