6f8b2bb295a31fc518e8997058eb0f2e852ce8bc
Luca Coviello <luca.coviello@gmail.com>
2018-01-16 23:05:59 +0100

    gctw per class

--- ctw/misc/extract_aligned_frames_from_indexes.py
+++ ctw/misc/extract_aligned_frames_from_indexes.py
@@ -21,11 +21,10 @@ def main():
         print video_folder
         print idx, video_folder
 
-        shutil.copyfile(source, dest)
-
         for frame_idx, frame in enumerate(aligned_idx[:, idx]):
             source = os.path.join(input_folder, video_folder, "image_{:05d}.jpg".format(frame))
             dest = os.path.join(output_folder, video_folder, "image_{:05d}_aligned.jpg".format(frame_idx + 1))
+            shutil.copyfile(source, dest)
 
 
 if __name__ == '__main__':
