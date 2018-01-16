56292f1660fc508086600cccb98ca1f71f00e30d
Luca Coviello <luca.coviello@gmail.com>
2018-01-16 23:00:22 +0100

    gctw per class

---
+++ ctw/misc/extract_aligned_frames_from_indexes.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+import numpy as np
+
+# TODO replicate the frames of the videos in image format
+
+def main():
+    input_folder = sys.argv[1]
+    alignment_file = sys.argv[2]
+    output_folder = sys.argv[3] if len(sys.argv) > 3 else input_folder
+
+    # matlab is 1-indexed
+    aligned_idx = np.loadtxt(alignment_file, delimiter=',').astype(np.int) - 1
+
+    video_folders = sorted(os.listdir(input_folder))
+    assert len(video_folders) == aligned_idx.shape[1], "lenght differs"
+
+    for idx, video_folder in enumerate(video_folders):
+        print idx, video_folder
+
+        shutil.copyfile(source, dest)
+
+        for frame_idx, frame in enumerate(aligned_idx[:, idx]):
+            source = os.path.join(input_folder, video_folder, "image_{:05d}.jpg".format(frame))
+            dest = os.path.join(output_folder, video_folder, "image_{:05d}_aligned.jpg".format(frame_idx + 1))
+
+
+if __name__ == '__main__':
+    main()
---
+++ ctw/misc/extract_aligned_frames_from_indexes_interface.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INPUT_FOLDER=$1
+ALIGNMENT_FOLDER=$2
+OUTPUT_FOLDER=$3
+
+for i in {01..43};
+do
+    python extract_aligned_frames_from_indexes.py ${INPUT_FOLDER}/$i/ "$ALIGNMENT_FOLDER/ali_gctw_deep_${i}_P.csv" $OUTPUT_FOLDER/
+done
