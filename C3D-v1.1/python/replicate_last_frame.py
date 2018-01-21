dd75230d95534c4d0439f64e96b6f5b9ae20e94e
Luca Coviello <luca.coviello@gmail.com>
2018-01-21 22:31:50 +0100

    Added script for copying files in class folders

--- C3D-v1.1/python/replicate_last_frame.py
+++ C3D-v1.1/python/replicate_last_frame.py
@@ -4,13 +4,12 @@ import shutil
 
 
 def main():
-    if len(sys.argv) != 4:
-        print "Usage: ./{} input_folder output_folder final".format(__file__)
+    if len(sys.argv) != 3:
+        print "Usage: ./{} input_folder final".format(__file__)
         sys.exit(-1)
 
     input_folder = sys.argv[1]
-    output_folder = sys.argv[2]
-    final_length = int(sys.argv[3])
+    final_length = int(sys.argv[2])
 
     video_folders = sorted(os.listdir(input_folder))
     for video_folder in video_folders:
@@ -21,7 +20,7 @@ def main():
         # remaining frames
         for i in range(final_length - len(frames)):
             frame_name = "image_{:05d}.jpg".format(len(frames) + i + 1)
-            dest = os.path.join(output_folder, video_folder, frame_name)
+            dest = os.path.join(input_folder, video_folder, frame_name)
             shutil.copyfile(source, dest)
 
 
