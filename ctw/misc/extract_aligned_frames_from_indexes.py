2c924afbef72927f2402db602ea7a80665389f33
Luca Coviello <luca.coviello@gmail.com>
2018-01-21 19:57:27 +0100

    new scripts for preprocessing

--- ctw/misc/extract_aligned_frames_from_indexes.py
+++ ctw/misc/extract_aligned_frames_from_indexes.py
@@ -26,7 +26,7 @@ def main():
 
         for frame_idx, frame in enumerate(aligned_idx[:, idx]):
             source = os.path.join(input_folder, video_folder,
-                                  "image_{:05d}.jpg".format(int(round(frame)) - 1))
+                                  "image_{:05d}.jpg".format(int(round(frame))))
             dest = os.path.join(output_folder, video_folder,
                                 "image_{:05d}.jpg".format(frame_idx + 1))
             shutil.copyfile(source, dest)
