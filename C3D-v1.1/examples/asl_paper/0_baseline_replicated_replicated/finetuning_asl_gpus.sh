17fe96328cab4d0f3dfc47b0a9576e3b03b308d3
Luca Coviello <luca.coviello@gmail.com>
2018-01-22 23:37:14 +0100

    Quick math

--- C3D-v1.1/examples/asl_paper/0_baseline_replicated_replicated/finetuning_asl_gpus.sh
+++ C3D-v1.1/examples/asl_paper/0_baseline_replicated_replicated/finetuning_asl_gpus.sh
@@ -3,13 +3,12 @@
 SOLVER_FILE=$1
 NUM_GPUS=$2
 
-if [ ! -f activitynet_iter_135000.caffemodel ]; then
+if [ ! -f c3d_resnet18_sports1m_r2_iter_2800000.caffemodel ]; then
     echo "activitynet_iter_135000 not found, exiting"
     exit 1
 fi
 
-mkdir -p LOG_TRAIN snapshots
-chmod 777 LOG_TRAIN snapshots
+mkdir -p LOG_TRAIN
 
 source /etc/profile.d/modules.sh
 module load cuda/8.0
@@ -20,12 +19,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
 CAFFE_HOME=/data/sparks/share/R-C3D/caffe3d/
 
 GPUS=
+ONE=1
+NUM_GPUS=$((NUM_GPUS-ONE))
 
-for i in `seq 1 $NUM_GPUS`;
+
+for i in `seq 0 $NUM_GPUS`;
 do
     GPUS=$GPUS,$i
 done
 GPUS=${GPUS:1}
 
 
-GLOG_log_dir="./LOG_TRAIN" $CAFFE_HOME/build/tools/caffe.bin train --solver=${SOLVER_FILE} --weights=activitynet_iter_135000.caffemodel --gpu=$GPUS
+echo GLOG_log_dir="./LOG_TRAIN" $CAFFE_HOME/build/tools/caffe.bin train --solver=${SOLVER_FILE} --weights=activitynet_iter_135000.caffemodel --gpu=$GPUS
