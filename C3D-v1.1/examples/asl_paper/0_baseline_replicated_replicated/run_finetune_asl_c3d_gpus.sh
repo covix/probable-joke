9b5d137d427f98c979f056a656bfb5a1b050d8fc
Luca Coviello <luca.coviello@gmail.com>
2018-01-22 23:05:20 +0100

    Added solvers for different numbers of GPUs

--- C3D-v1.1/examples/asl_paper/0_baseline_replicated_replicated/run_finetune_asl_c3d_gpus.sh
+++ C3D-v1.1/examples/asl_paper/0_baseline_replicated_replicated/run_finetune_asl_c3d_gpus.sh
@@ -1,4 +1,4 @@
 SOLVER_FILE=$1
 NUM_GPUS=$2
 
-oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" -l /nodes=1/gpunum=4,walltime=12 -t besteffort -S "./finetuning_asl_gpus.sh $SOLVER_FILE $NUM_GPUS"
+oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" -l /nodes=1/gpunum=$NUM_GPUS,walltime=12 -t besteffort -S "./finetuning_asl_gpus.sh $SOLVER_FILE $NUM_GPUS"
