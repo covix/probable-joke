b878708702ae883aef884fb265ae8e856ef95eb7
Luca Coviello <luca.coviello@gmail.com>
2018-01-26 00:58:49 +0100

    Updated finetuning script

--- pipeline/4.0/alignment/align_deep_pca_gctw_interface.sh
+++ pipeline/4.0/alignment/align_deep_pca_gctw_interface.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 INPUT_FOLDER=$1
-TEST_SAMPLE=$2
-OUTPUT_FOLDER=$3
+CLASS=$2
+TEST_SAMPLE=$3
+OUTPUT_FOLDER=$4
 
-for i in {01..43};
-do
+ENR_CODE=`dirname $0`  # Current folder
 
-    ENR_CODE=`dirname $0`  # Current folder
+CMD="\
+    addpath(genpath([cd '/' '$ENR_CODE'])); \
+    align_deep_pca_gctw_class_test('${INPUT_FOLDER}/${CLASS}', '$CLASS', '$TEST_SAMPLE', '$OUTPUT_FOLDER', '$ENR_CODE'); \
+    exit;
+"
 
-    CMD="\
-        addpath(genpath([cd '/' '$ENR_CODE'])); \
-        align_deep_pca_gctw_class_test('${INPUT_FOLDER}/${i}', '$i', '$TEST_SAMPLE', '$OUTPUT_FOLDER', '$ENR_CODE'); \
-        exit;
-    "
+$MATLAB -r "$CMD" # > gctw.log 2>&1
 
-    $MATLAB -r "$CMD" # > gctw.log 2>&1
 
-done
+# Usage:
+# ./run_align_deep_pca_gctw.sh ../../../../experiments/datasets/train/pca_features_train/ 01 ../../../../experiments/datasets/test/pca_features_test/01-M-01-C-comp_features.csv .
