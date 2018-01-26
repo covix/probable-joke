f6f5d56f8c185a26889d4fe7f3f4a535846da481
Luca Coviello <luca.coviello@gmail.com>
2018-01-26 01:12:29 +0100

    Added gctw per class at test time

--- pipeline/4.0/alignment/run_align_deep_pca_gctw.sh
+++ pipeline/4.0/alignment/run_align_deep_pca_gctw.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 INPUT_FOLDER=$1
-TEST_SAMPLE=$2
-OUTPUT_FOLDER=$3
+CLASS=$2
+TEST_SAMPLE=$3
+OUTPUT_FOLDER=$4
 
 ENR_CODE=`dirname $0`  # Current folder, containing supporting scripts and files
 
 source ${ENR_CODE}/../config.sh
 
-oarsub -l /core=1 -S "${ENR_CODE}/align_deep_pca_gctw_interface.sh $INPUT_FOLDER $TEST_SAMPLE $OUTPUT_FOLDER"
+oarsub -l /core=1 -S "${ENR_CODE}/align_deep_pca_gctw_interface.sh $INPUT_FOLDER $CLASS $TEST_SAMPLE $OUTPUT_FOLDER"
