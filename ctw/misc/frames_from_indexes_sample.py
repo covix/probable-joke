78ce011ea641556730474bf34255d377f5e2e9d6
Anto95 <tonio.db@hotmail.it>
2018-01-26 21:18:38 +0100

    ogosh

--- ctw/misc/frames_from_indexes_sample.py
+++ ctw/misc/frames_from_indexes_sample.py
@@ -17,7 +17,9 @@ def main():
     for frame_idx, frame in enumerate(indexes):
         source = os.path.join(input_folder, video_folder,
                               "image_{:05d}.jpg".format(int(round(frame))))
+        print("Source: ",source)
         dest = os.path.join(output_folder, video_folder,"image_{:05d}.jpg".format(frame_idx + 1))
+        print("Destination: ",dest)
         shutil.copyfile(source, dest)
 
 if __name__ == '__main__':
