896bf2f42133475a484db36704bdb6847ab0bbc1
Emily Soehler <emilysoehler@web.de>
2018-01-30 13:50:33 +0100

    adapt input parameters

---
+++ C3D-v1.1/examples/asl_paper/TEST/softmax-examination/testing_asl_gpus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+MODEL_FILE=$1
+WEIGHTS=$2
+NUM_GPUS=$3
+
+
+mkdir -p LOG_TRAIN snapshots
+chmod 777 LOG_TRAIN snapshots
+
+source /etc/profile.d/modules.sh
+module load cuda/8.0
+module load cudnn/5.1-cuda-8.0
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/sparks/share/nccl-master/lib
+
+CAFFE_HOME=/data/sparks/share/R-C3D/caffe3d/
+
+GPUS=
+ONE=1
+NUM_GPUS=$((NUM_GPUS-ONE))
+
+
+for i in `seq 0 $NUM_GPUS`;
+do
+    GPUS=$GPUS,$i
+done
+GPUS=${GPUS:1}
+
+
+GLOG_log_dir="./LOG_TRAIN" $CAFFE_HOME/build/tools/caffe.bin test --model=$MODEL_FILE --weights=$WEIGHTS --gpu=$GPUS -iterations 6
