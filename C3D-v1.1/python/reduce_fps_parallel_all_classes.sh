4fa1bafade63e9c80ee747b33dbbdbc04f6e2cdf
Anto95 <tonio.db@hotmail.it>
2018-01-30 14:55:34 +0100

    error

--- C3D-v1.1/python/reduce_fps_parallel_all_classes.sh
+++ C3D-v1.1/python/reduce_fps_parallel_all_classes.sh
@@ -2,9 +2,10 @@ INPUT_FOLDER=$1
 OUTPUT_FOLDER=$2
 
 for f in `ls $INPUT_FOLDER`;
-mkdir $OUTPUT_FOLDER/$f
 do
-  for i in $f
-  mkdir $OUTPUT_FOLDER/$f/$i
-  echo python reduce_fps_parallel.py $INPUT_FOLDER $OUTPUT_FOLDER/$f/$i 7.5
+  mkdir $OUTPUT_FOLDER/$f
+  for i in $f;
+    mkdir $OUTPUT_FOLDER/$f/$i
+    echo python reduce_fps_parallel.py $INPUT_FOLDER $OUTPUT_FOLDER/$f/$i 7.5
+done
 done
