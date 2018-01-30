ddc3724e83f111a8503d20811dc4f4d9ab9c426d
Anto95 <tonio.db@hotmail.it>
2018-01-30 15:14:33 +0100

    error-3

--- C3D-v1.1/python/replicate_last_frame_parallel_all_classes.sh
+++ C3D-v1.1/python/replicate_last_frame_parallel_all_classes.sh
@@ -4,6 +4,6 @@ for f in `ls $INPUT_FOLDER`;
 do
   for i in `ls $INPUT_FOLDER/$f`;
   do
-    python replicate_last_frame_parallel_all_classes.py $INPUT_FOLDER/$f/$i 45
+    python replicate_last_frame_parallel.py $INPUT_FOLDER/$f/$i 45
   done
 done
