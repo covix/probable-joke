dd97aa3af799713e6a8e225019a8dadaf8508593
Emily Soehler <emilysoehler@web.de>
2018-01-30 14:22:05 +0100

    Added parameter

--- C3D-v1.1/examples/asl_paper/TEST/softmax-examination/run_test_c3d_gpus.sh
+++ C3D-v1.1/examples/asl_paper/TEST/softmax-examination/run_test_c3d_gpus.sh
@@ -1,4 +1,5 @@
 MODEL_FILE=$1
+WEIGHTS=$2
 NUM_GPUS=1
 
 JOB_NAME=${MODEL_FILE%.*}
@@ -7,4 +8,4 @@ oarsub -p "gpu='YES' and gpucapability >= '5.2' and gpumem >= '10000'" \
     -l /nodes=1/gpunum=$NUM_GPUS,walltime=12 \
     -t besteffort \
     -n $JOB_NAME \
-    -S "./testing_asl_gpus.sh $MODEL_FILE $NUM_GPUS"
+    -S "./testing_asl_gpus.sh $MODEL_FILE $WEIGHTS"
