cf1e0c4c0da7ab83242fadf18f91c00eb8da3dfc
Anto95 <tonio.db@hotmail.it>
2018-02-03 18:54:31 +0100

    take out silence frame script

--- C3D-v1.1/scripts/clean_silence_frames.sh
+++ C3D-v1.1/scripts/clean_silence_frames.sh
@@ -3,13 +3,13 @@ VIDEO_FOLDER=$2
 OUTPUT_FOLDER=$3
 for f in `ls $FEATURES_FOLDER`;
 do
-	indexes=`python script.py $FEATURES_FOLDER/$f/$f_features.csv`
-  start=`cut -d" " -f1<<<$indexes`
-  end=`cut -d" " -f2<<<$indexes`
-  frames=`ls $VIDEO_FOLDER/$f`
-  frames=`cut -d" " -f$start,$end <<<$frames`
-  mkdir -p $OUTPUT_FOLDER/$f
-  cp $frames $OUTPUT_FOLDER/$f
+	echo indexes=`python script.py $FEATURES_FOLDER/$f/$f_features.csv`
+  echo start=`cut -d" " -f1<<<$indexes`
+  echo end=`cut -d" " -f2<<<$indexes`
+  echo frames=`ls $VIDEO_FOLDER/$f`
+  echo frames=`cut -d" " -f$start,$end <<<$frames`
+  echo mkdir -p $OUTPUT_FOLDER/$f
+  echo cp $frames $OUTPUT_FOLDER/$f
 done
 
 
