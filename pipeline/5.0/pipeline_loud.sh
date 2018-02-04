09f6f108609205079b3ce863aa38e77c9b7eb7e0
Anto95 <tonio.db@hotmail.it>
2018-02-04 20:39:22 +0100

    scarf

--- pipeline/5.0/pipeline_loud.sh
+++ pipeline/5.0/pipeline_loud.sh
@@ -10,7 +10,7 @@ LOUD_FRAMES_TRAIN=$TRAIN_FOLDER/loud_train
 ORIGINAL_FRAMES_TEST=$TEST_FOLDER/original_test
 ORIGINAL_FEATURES_TEST=$TEST_FOLDER/deep_features_test
 LOUD_FEATURES_TEST=$TEST_FOLDER/deep_features_loud_test
-LOUD_FRAMES_TEST=$TRAIN_FOLDER/loud_test
+LOUD_FRAMES_TEST=$TEST_FOLDER/loud_test
 
 LOUD_FRAMES_CLASS_TRAIN=$TRAIN_FOLDER/loud_frames_class_train
 LOUD_FRAMES_CLASS_TEST=$TEST_FOLDER/loud_frames_class_test
@@ -25,7 +25,7 @@ sh $SCRIPTS_FOLDER/clean_silence_features.sh $ORIGINAL_FEATURES_TRAIN $LOUD_FEAT
 
 #Create loud_test
 echo "Create loud_test..."
-sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FEATURES_TRAIN $ORIGINAL_FRAMES_TEST $LOUD_FRAMES_TEST
+sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FEATURES_TEST $ORIGINAL_FRAMES_TEST $LOUD_FRAMES_TEST
 #Create loud_test_features
 echo "Create loud_test_features..."
 sh $SCRIPTS_FOLDER/clean_silence_features.sh $ORIGINAL_FEATURES_TEST $LOUD_FEATURES_TEST
@@ -48,5 +48,3 @@ sh $SCRIPTS_FOLDER/move_frames.sh $LOUD_FRAMES_TEST $LOUD_FRAMES_CLASS_TEST
 
 
 #--- Alignment --------------------------------------------------------------
-
-
