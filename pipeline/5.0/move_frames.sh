2bba81e445d7efe14046d76bd33c38937d27b954
schenock <mitrevdane@gmail.com>
2018-02-04 20:19:48 +0100

    5 minutes for this script

---
+++ pipeline/5.0/move_frames.sh
@@ -0,0 +1,21 @@
+
+INPUT_FOLDER=$1
+OUTPUT_FOLDER=$2
+
+
+# Create output folder
+echo "Creating output folder.."
+mkdir $OUTPUT_FOLDER
+
+# Moving by class
+echo "Moving the frames by class.."
+for class in {01..43};
+do
+mkdir $OUTPUT_FOLDER/$class
+for f in `ls -d *M-$class-*`;
+do
+        cp -r $INPUT_FOLDER/$f $OUTPUT_FOLDER/$class
+done
+        echo "-----"
+done
+
--- pipeline/5.0/pipeline_loud.sh
+++ pipeline/5.0/pipeline_loud.sh
@@ -12,17 +12,22 @@ ORIGINAL_FEATURES_TEST=$TEST_FOLDER/deep_features_test
 LOUD_FEATURES_TEST=$TEST_FOLDER/deep_features_loud_test
 LOUD_FRAMES_TEST=$TRAIN_FOLDER/loud_test
 
+LOUD_FRAMES_CLASS_TRAIN=$TRAIN_FOLDER/loud_frames_class_train
+LOUD_FRAMES_CLASS_TEST=$TEST_FOLDER/loud_frames_class_test
+
+
+
 
 #Create loud_train
 echo "Create loud_train..."
-sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FEATURES_TRAIN $ORIGINAL_FRAMES_TRAIN $LOUD_FRAMES_TRAIN
+sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FRAMES_TRAIN $LOUD_FRAMES_TRAIN
 echo "Create loud_train_features..."
 #Create loud_train_features
 sh $SCRIPTS_FOLDER/clean_silence_features.sh $ORIGINAL_FEATURES_TRAIN $LOUD_FEATURES_TRAIN
 
 #Create loud_test
 echo "Create loud_test..."
-sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FEATURES_TRAIN $ORIGINAL_FRAMES_TEST $LOUD_FRAMES_TEST
+sh $SCRIPTS_FOLDER/clean_silence_frames.sh $ORIGINAL_FRAMES_TEST $LOUD_FRAMES_TEST
 #Create loud_test_features
 echo "Create loud_test_features..."
 sh $SCRIPTS_FOLDER/clean_silence_features.sh $ORIGINAL_FEATURES_TEST $LOUD_FEATURES_TEST
@@ -31,6 +36,20 @@ chmod 777 $LOUD_FRAMES_TRAIN
 chmod 777 $LOUD_FEATURES_TRAIN
 chmod 777 $LOUD_FRAMES_TEST
 chmod 777 $LOUD_FEATURES_TEST
-echo "Alignment boh"
-#------------------------------------------------------------------------
-#Alignment...
+
+
+
+#--- Moving -------------------------------------------------------------
+
+# Moving by class
+echo "Moving frames by class.."
+sh $SCRIPTS_FOLDER/move_frames.sh $LOUD_FRAMES_TRAIN $LOUD_FRAMES_CLASS_TRAIN
+
+# Moving by class test
+echo "Moving frames by class test.."
+sh $SCRIPTS_FOLDER/move_frames.sh $LOUD_FRAMES_TEST $LOUD_FRAMES_CLASS_TEST
+
+
+#--- Alignment --------------------------------------------------------------
+
+
