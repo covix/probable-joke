a28d517b4a85d8ac967d46d3a5f70a48eb4093db
Anto95 <tonio.db@hotmail.it>
2018-02-04 19:54:28 +0100

    pipelineee

--- C3D-v1.1/scripts/clean_silence_features.sh
+++ C3D-v1.1/scripts/clean_silence_features.sh
@@ -1,7 +1,7 @@
 FEATURES_FOLDER=$1
 OUTPUT_FOLDER=$2
 suffix=_features.csv
-REMOVE_SILENT_FEATURES_PATH=/data/sparks/share/asl/experiments/datasets/scripts_data/remove_silent_features.py
+REMOVE_SILENT_FEATURES_PATH=/data/sparks/share/asl/experiments/datasets/scripts_data/remove_silent_frames.py
 
 for f in `ls $FEATURES_FOLDER`;
 do
