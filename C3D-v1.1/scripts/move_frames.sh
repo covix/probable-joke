75ad8cd9e8b28ce35b3a56d9e6d3dbce65ab8489
Emily Soehler <emilysoehler@web.de>
2018-02-04 21:50:31 +0100

    i get a grapefruit from dane

--- C3D-v1.1/scripts/move_frames.sh
+++ C3D-v1.1/scripts/move_frames.sh
@@ -12,10 +12,9 @@ echo "Moving the frames by class.."
 for class in {01..43};
 do
 mkdir $OUTPUT_FOLDER/$class
-for f in `ls -d *M-$class-*`;
+for f in `ls -d $INPUT_FOLDER/*M-$class-*`;
 do
         cp -r $INPUT_FOLDER/$f $OUTPUT_FOLDER/$class
 done
         echo "-----"
 done
-
