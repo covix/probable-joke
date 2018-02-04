9e606010094fec5d0fd1698af5942a49cb15a9ad
Anto95 <tonio.db@hotmail.it>
2018-02-04 15:46:57 +0100

    First experiment on loud and script

--- C3D-v1.1/scripts/clean_silence_frames.sh
+++ C3D-v1.1/scripts/clean_silence_frames.sh
@@ -1,17 +1,30 @@
 FEATURES_FOLDER=$1
 VIDEO_FOLDER=$2
 OUTPUT_FOLDER=$3
+suffix=_features.csv
 for f in `ls $FEATURES_FOLDER`;
 do
-  indexes=`python script.py $FEATURES_FOLDER/$f/$f_features.csv`
-  echo start=`cut -d" " -f1<<<$indexes`
-  echo end=`cut -d" " -f2<<<$indexes`
-  echo frames=`ls $VIDEO_FOLDER/$f`
-  echo frames=`cut -d" " -f$start,$end <<<$frames`
-  echo mkdir -p $OUTPUT_FOLDER/$f
-  echo cp $frames $OUTPUT_FOLDER/$f
+  echo video $f
+  indexes=`python remove_silent_frames.py $FEATURES_FOLDER/$f/$f$suffix`
+  indexes=${indexes:1:-1}
+  ind1=`cut -d"," -f1 <<<$indexes`
+  ind2=`cut -d"," -f2 <<<$indexes`
+  start=`cut -d" " -f1<<<$ind1`
+  end=`cut -d" " -f2<<<$ind2`
+  frames=`ls $VIDEO_FOLDER/$f`
+  frames=`cut -d" " -f$start-$end <<<$frames`
+  mkdir -p $OUTPUT_FOLDER/$f
+  a=1
+  for i in $frames;
+  do
+    new=$(printf "image_%05d.jpg" "$a")
+    cp $VIDEO_FOLDER/$f/$i $OUTPUT_FOLDER/$f/$new
+    let a=a+1
+  done
 done
 
 
+
 #Usage example
-#./clean_silence_frame /data/sparks/share/asl/experiments/datasets/train/deep_features_train /data/sparks/share/asl/experiments/datasets/train/original_train /data/sparks/share/asl/experiments/datasets/train/loud_train
+#./clean_silence_frame.sh /data/sparks/share/asl/experiments/datasets/train/dee$
+#/data/sparks/share/asl/experiments/datasets/train/original_train /data/sparks/$
