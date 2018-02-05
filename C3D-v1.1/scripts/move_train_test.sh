9e690c65d686e3d4a7f81424767b2595174e9f53
Anto95 <tonio.db@hotmail.it>
2018-02-05 20:15:00 +0100

    pre-pipeline

--- C3D-v1.1/scripts/move_train_test.sh
+++ C3D-v1.1/scripts/move_train_test.sh
@@ -2,7 +2,7 @@
 LABELS_FILE=$1
 DATA_FOLDER=$2
 OUTPUT_FOLDER=$3
-for l in cat $LABELS_FILE | cut -d' ' -f1;
+for l in `cat $LABELS_FILE | cut -d' ' -f1`;
 do
-  echo cp -r $DATA_FOLDER/$l $OUTPUT_FOLDER/$l
+  cp -r $DATA_FOLDER/$l $OUTPUT_FOLDER/$l
 done
