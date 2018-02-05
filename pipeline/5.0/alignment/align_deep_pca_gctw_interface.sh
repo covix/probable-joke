b22753784b4ec8448eda544e4000fb72ad01f116
Luca Coviello <luca.coviello@gmail.com>
2018-02-05 17:06:09 +0100

    testing before rolling

--- pipeline/5.0/alignment/align_deep_pca_gctw_interface.sh
+++ pipeline/5.0/alignment/align_deep_pca_gctw_interface.sh
@@ -5,13 +5,15 @@ OUTPUT_FOLDER=$2
 
 MATLAB=/usr/local/bin/matlab2017a
 
-for i in {01..43};
+echo "Alert: aligning only one class"
+for i in {01..01};
 do
 
     ENR_CODE=`dirname $0`  # Current folder
+    echo $ENR_CODE
 
     CMD="\
-        addpath(genpath([$ENR_CODE'])); \
+        addpath(genpath(['$ENR_CODE'])); \
         align_deep_pca_gctw_class('${INPUT_FOLDER}/${i}', '$i', '$OUTPUT_FOLDER', '$ENR_CODE'); \
         exit;
     "
@@ -19,3 +21,7 @@ do
     $MATLAB -r "$CMD" # > gctw.log 2>&1
 
 done
+
+
+
+addpath(genpath([cd '/' '/data/sparks/share/asl/probable-joke/pipeline/5.0/alignment']));
