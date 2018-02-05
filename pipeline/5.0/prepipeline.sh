da3f9bc2f5d787b9a66be91158565d10a874e054
Anto95 <tonio.db@hotmail.it>
2018-02-05 20:04:38 +0100

    pre-pipeline

--- pipeline/5.0/prepipeline.sh
+++ pipeline/5.0/prepipeline.sh
@@ -1,9 +1,9 @@
-$VIDEO_FOLDER=
-$FRAMES_FOLDER=
-$TRAIN_LABELS=
-$TEST_LABELS=
-$TRAIN_FOLDER=
-$TEST_FOLDER=
+$VIDEO_FOLDER=/data/sparks/share/asl/data/VideoThumbnailsMotion/
+$FRAMES_FOLDER=/data/sparks/share/asl/data/asl_png
+$TRAIN_LABELS=/data/sparks/share/asl/experiments/datasets/train.txt
+$TEST_LABELS=/data/sparks/share/asl/experiments/datasets/test.txt
+$TRAIN_FOLDER=/data/sparks/share/asl/experiments/datasets/original_train
+$TEST_FOLDER=/data/sparks/share/asl/experiments/datasets/original_test
 
 $SCRIPTS_FOLDER=/data/sparks/share/asl/probable-joke/C3D-v1.1/scripts
 
@@ -15,6 +15,7 @@ set -e
 if [[ ! -d $FRAMES_FOLDER ]]; then
     #Extract frames
     echo "Create extract frames"
+    mkdir -p $FRAMES_FOLDER
     sh extract_frames.sh $VIDEO_FOLDER $FRAMES_FOLDER
     chmod 777 $FRAMES_FOLDER
 else
@@ -24,6 +25,7 @@ fi
 if [[ ! -d $TRAIN_FOLDER ]]; then
     #Create original_train
     echo "Create original_train..."
+    mkdir -p $TRAIN_FOLDER
     sh $SCRIPTS_FOLDER/move_train_test.sh $TRAIN_LABELS $FRAMES_FOLDER $TRAIN_FOLDER
     chmod 777 $TRAIN_FOLDER
 else
@@ -33,6 +35,7 @@ fi
 if [[ ! -d $TEST_FOLDER ]]; then
     #Create original_test
     echo "Create original_test"
+    mkdir -p $TEST_FOLDER
     sh $SCRIPTS_FOLDER/move_train_test.sh $TEST_LABELS $FRAMES_FOLDER $TEST_FOLDER
     chmod 777 $TEST_FOLDER
 else
