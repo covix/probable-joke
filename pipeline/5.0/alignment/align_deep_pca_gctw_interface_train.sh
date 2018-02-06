6c265fd9348944621f34d71de30858ff7e7535cf
Luca Coviello <luca.coviello@gmail.com>
2018-02-06 21:07:44 +0100

    Pipeline working till alignment

--- pipeline/5.0/alignment/align_deep_pca_gctw_interface_train.sh
+++ pipeline/5.0/alignment/align_deep_pca_gctw_interface_train.sh
@@ -5,17 +5,23 @@ OUTPUT_FOLDER=$2
 
 MATLAB=/usr/local/bin/matlab2017a
 
+> gctw.log
+
 for i in {01..43};
 do
     echo Aligning class $i
+    echo Aligning class $i > gctw.log
     ENR_CODE=`dirname $0`  # Current folder
 
     CMD="\
-        addpath(genpath(['$ENR_CODE'])); \
-        align_deep_pca_gctw_class_train('${INPUT_FOLDER}/${i}', '$i', '$OUTPUT_FOLDER', '$ENR_CODE'); \
+        try; \
+            addpath(genpath(['$ENR_CODE'])); \
+            align_deep_pca_gctw_class_train('${INPUT_FOLDER}/${i}', '$i', '$OUTPUT_FOLDER', '$ENR_CODE'); \
+        catch; \
+        end; \
         exit;
     "
 
-    $MATLAB -r "$CMD" # > gctw.log 2>&1
+    $MATLAB -r "$CMD" >> gctw.log 2>&1
 
 done
--- pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
+++ pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
@@ -4,6 +4,7 @@ TRAIN_FOLDER=$1  # organised in classes
 TEST_FOLDER=$2  # organised in classes
 OUTPUT_FOLDER=$3
 
+ENR_CODE=`dirname $0`  # Current folder, containing supporting scripts and files
 
 for CLASS_FOLDER in `ls $TRAIN_FOLDER`
 do
@@ -14,6 +15,6 @@ do
     for TEST_SAMPLE in `ls $TEST_FOLDER/$CLASS_FOLDER`
     do
         echo "Sample " $TEST_SAMPLE " is being aligned.."
-        ./run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/$TEST_SAMPLE $OUTPUT_FOLDER/$CLASS_FOLDER
+        ${ENR_CODE}/run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/$TEST_SAMPLE $OUTPUT_FOLDER/$CLASS_FOLDER
     done
 done
