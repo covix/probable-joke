1ad977960209fac4129fd92625d62c475154b02a
Luca Coviello <luca.coviello@gmail.com>
2018-02-07 23:26:05 +0100

    Take me down

--- C3D-v1.1/scripts/move_frames.sh
+++ C3D-v1.1/scripts/move_frames.sh
@@ -5,16 +5,16 @@ OUTPUT_FOLDER=$2
 
 # Create output folder
 echo "Creating output folder.."
-mkdir $OUTPUT_FOLDER
+mkdir -p $OUTPUT_FOLDER
 
 # Moving by class
 echo "Moving the frames by class.."
 for class in {01..43};
-do
-mkdir $OUTPUT_FOLDER/$class
-for f in `ls -d $INPUT_FOLDER/*M-$class-*`;
-do
+    do
+    mkdir -p $OUTPUT_FOLDER/$class
+    for f in `ls -d $INPUT_FOLDER/*M-$class-*`;
+    do
         cp -r $f $OUTPUT_FOLDER/$class
-done
-        echo "-----"
+    done
+    echo "-----"
 done
