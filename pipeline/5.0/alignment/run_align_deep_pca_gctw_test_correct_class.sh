7fe8bb12de3dfcf0d5f7ebdde704b89c5fdb1ae5
Luca Coviello <luca.coviello@gmail.com>
2018-02-07 15:03:21 +0100

    pushing laignment

--- pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
+++ pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
@@ -6,16 +6,20 @@ OUTPUT_FOLDER=$3
 
 ENR_CODE=`dirname $0`  # Current folder, containing supporting scripts and files
 
+ID=
+
 for CLASS_FOLDER in `ls $TRAIN_FOLDER`
 do
     echo "Started alignment for class " $class_folder 
     echo
     mkdir -p $OUTPUT_FOLDER/$CLASS_FOLDER
+    WAIT_ID=$ID
+    echo "WAIT_ID is: $WAIT_ID"
 
     for TEST_SAMPLE in `ls $TEST_FOLDER/$CLASS_FOLDER`
     do
         echo "Sample " $TEST_SAMPLE " is being aligned.."
-        CMD="${ENR_CODE}/run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/$TEST_SAMPLE $OUTPUT_FOLDER/$CLASS_FOLDER"
+        CMD="${ENR_CODE}/run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/$TEST_SAMPLE $OUTPUT_FOLDER/$CLASS_FOLDER $WAIT_ID"
         ID=$($CMD)
         echo $ID
     done
