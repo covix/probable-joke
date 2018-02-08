e1ce89d1fb0bb850c9f31a1af02015e4e7039c14
Luca Coviello <luca.coviello@gmail.com>
2018-02-08 22:40:27 +0100

    debug alignment

--- pipeline/5.0/alignment/align_deep_pca_gctw_class_test.m
+++ pipeline/5.0/alignment/align_deep_pca_gctw_class_test.m
@@ -4,39 +4,42 @@ function align_deep_pca_gctw_class_test(inputFolder, class, testSample, outputFo
 % History
 %   create  -  Luca Coviello (luca.coviello@gmail.com), 01-16-2018
 
-[filepath,name,ext] = fileparts(testSample);
-outfile = strcat(outputFolder, "/", name, '.csv');
+[filepath, name, ext] = fileparts(testSample);
+outfile = strcat(outputFolder, '/', name, '.csv');
 if exist(outfile, 'file') == 2
+    disp('im in the if!');
     exit
 end
 
-%% add libraries path
-footpath = cd;
-addpath(genpath([footpath '/' ctwFolder '/ctw/src']));
-addpath(genpath([footpath '/' ctwFolder '/ctw/lib']));
+exit
+disp('exited');
 
+% %% add libraries path
+% footpath = cd;
+% addpath(genpath([footpath '/' ctwFolder '/ctw/src']));
+% addpath(genpath([footpath '/' ctwFolder '/ctw/lib']));
 
-%% algorithm parameter
-parCca = st('d', 0.95, 'lams', .1); % CCA: reduce dimension to keep at least 0.95 energy
-parGN = st('nItMa', 2, 'inp', 'linear'); % Gauss-Newton: 2 iterations to update the weight in GTW,
-parGtw = st('nItMa', 100);
+% %% algorithm parameter
+% parCca = st('d', 0.95, 'lams', .1); % CCA: reduce dimension to keep at least 0.95 energy
+% parGN = st('nItMa', 2, 'inp', 'linear'); % Gauss-Newton: 2 iterations to update the weight in GTW,
+% parGtw = st('nItMa', 100);
 
-%% data
-aslData = aslAliDataFeaturesPlusSample(inputFolder, testSample);
-X0s = aslData.DPs;
-Xs = pcas(X0s, st('d', min(cellDim(X0s, 1)), 'cat', 'n'));
+% %% data
+% aslData = aslAliDataFeaturesPlusSample(inputFolder, testSample);
+% X0s = aslData.DPs;
+% Xs = pcas(X0s, st('d', min(cellDim(X0s, 1)), 'cat', 'n'));
 
-%% monotonic basis
-ns = cellDim(Xs, 2);
-l = round(max(ns) * 1.1);
-bas = baTems(l, ns, 'pol', [5 .5], 'tan', [5 1 1]); % 2 polynomial and 3 tangent functions
+% %% monotonic basis
+% ns = cellDim(Xs, 2);
+% l = round(max(ns) * 1.1);
+% bas = baTems(l, ns, 'pol', [5 .5], 'tan', [5 1 1]); % 2 polynomial and 3 tangent functions
 
-%% utw (initialization)
-aliUtw = utw(Xs, bas, []);
+% %% utw (initialization)
+% aliUtw = utw(Xs, bas, []);
 
-%% gctw
-aliGtw = gtw(Xs, bas, aliUtw, [], parGtw, parCca, parGN);
+% %% gctw
+% aliGtw = gtw(Xs, bas, aliUtw, [], parGtw, parCca, parGN);
 
-%% save indexes
-P = aliGtw.P;
-csvwrite(outfile, P)
+% %% save indexes
+% P = aliGtw.P;
+% csvwrite(outfile, P)
