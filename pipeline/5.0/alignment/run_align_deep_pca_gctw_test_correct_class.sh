d592fd291d9a012e9df078a597003fc056ee5d20
Luca Coviello <luca.coviello@gmail.com>
2018-02-08 01:21:45 +0100

    stiamo imbrogliando

--- pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
+++ pipeline/5.0/alignment/run_align_deep_pca_gctw_test_correct_class.sh
@@ -3,29 +3,30 @@
 TRAIN_FOLDER=$1  # organised in classes
 TEST_FOLDER=$2  # organised in classes
 OUTPUT_FOLDER=$3
-ID1=$4
-ID2=$5
+ORIGINAL_FOLDER=$4  # stiamo imbrogliando
+ID1=$5
+ID2=$6
 
 ID="$ID1:$ID2"
 
 ENR_CODE=`dirname $0`  # Current folder, containing supporting scripts and files
 
-for CLASS_FOLDER in `ls $TRAIN_FOLDER`
+for CLASS_FOLDER in {01..43}
 do
-    echo "Started alignment for class " $class_folder 
+    echo "Queueing alignment for class " $CLASS_FOLDER
     echo
     mkdir -p $OUTPUT_FOLDER/$CLASS_FOLDER
     WAIT_ID=$ID
     ID=
     echo "WAIT_ID is: $WAIT_ID"
 
-    for TEST_SAMPLE in `ls $TEST_FOLDER/$CLASS_FOLDER`
+    for TEST_SAMPLE in `ls $ORIGINAL_FOLDER | grep "\-$CLASS_FOLDER\-"`
     do
-        echo "Sample " $TEST_SAMPLE " is being aligned.."
-        CMD="${ENR_CODE}/run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/$TEST_SAMPLE $OUTPUT_FOLDER/$CLASS_FOLDER $WAIT_ID"
+        echo "Sample $TEST_SAMPLE will be aligned.."
+        CMD="${ENR_CODE}/run_align_deep_pca_gctw_test.sh $TRAIN_FOLDER $CLASS_FOLDER $TEST_FOLDER/$CLASS_FOLDER/${TEST_SAMPLE}_features.csv $OUTPUT_FOLDER/$CLASS_FOLDER $WAIT_ID"
         ID="--anterior=$($CMD):$ID"
         echo $ID
     done
 done
 
-echo -e "ANTERIOR=$ID"
+echo "ANTERIOR=$ID"
