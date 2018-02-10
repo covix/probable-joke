1bc0a6e1f0f2c9a8de8a50ccbb9441732bf5a901
Luca Coviello <luca.coviello@gmail.com>
2018-02-10 14:20:26 +0100

    Added script to generate labels

--- C3D-v1.1/examples/asl_paper2/TEST/softmax-examination/generate_label_files.sh
+++ C3D-v1.1/examples/asl_paper2/TEST/softmax-examination/generate_label_files.sh
@@ -7,19 +7,23 @@ ONE=1
 
 mkdir -p $OUTPUT_FOLDER
 
-for CLASS_ID in {01..43}
+for CLASS_ID in {1..43}
 do
     CLASS_ID_MINUS_1=$((CLASS_ID-ONE))
-    LABEL_FILE="$OUTPUT_FOLDER/c_${CLASS_ID}"
+    LABEL_FILE="$OUTPUT_FOLDER/c_${CLASS_ID_PADDED}.txt"
+    
+    # pad the number 
+    CLASS_ID_PADDED=`seq -f '%02g' $CLASS_ID $CLASS_ID`
+
     > $LABEL_FILE
 
-    for SAMPLE in `ls $INPUT_FOLDER | grep "\-$CLASS_ID"`
+    for SAMPLE in `ls $INPUT_FOLDER | grep "\-$CLASS_ID_PADDED"`
     do
         echo $SAMPLE $FRAME_IDX $CLASS_ID_MINUS_1 $PAD > $LABEL_FILE
     done
 done
 
 # Usage example: 
-# ./aligned_reduced_fps_loud_test \
+# ./generate_label_files.sh \
 #     /data/sparks/share/asl/experiments/datasets/test/aligned_reduced_fps_loud_test \
 #     /data/sparks/share/asl/experiments/datasets/test/aligned_reduced_fps_loud_test_class_labels
