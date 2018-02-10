9282facf009866af8f6b5a43ac51373ca71b69ae
Luca Coviello <luca.coviello@gmail.com>
2018-02-10 11:42:56 +0100

    Bla

---
+++ C3D-v1.1/python/reduce_fps_parallel_all_classes_new.sh
@@ -0,0 +1,14 @@
+INPUT_FOLDER=$1
+OUTPUT_FOLDER=$2
+
+ENR_DIR=`dirname $0`
+for f in `ls $INPUT_FOLDER`;
+do
+  mkdir -p $OUTPUT_FOLDER/$f
+  python $ENR_DIR/reduce_fps_parallel.py $INPUT_FOLDER/$f $OUTPUT_FOLDER/$f 7.5
+  # for i in `ls $INPUT_FOLDER/$f`;
+  # do
+  #   mkdir -p $OUTPUT_FOLDER/$f/$i
+  #   python $ENR_DIR/reduce_fps_parallel.py $INPUT_FOLDER/$f/$i $OUTPUT_FOLDER/$f/$i 7.5
+  # done
+done
