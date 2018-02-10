6183dd0898ca1bf2ff9fedb70a4428fb84c16477
Anto95 <tonio.db@hotmail.it>
2018-02-10 17:53:51 +0100

    yeeee

--- C3D-v1.1/python/replicate_last_frame_parallel_all_classes_new.sh
+++ C3D-v1.1/python/replicate_last_frame_parallel_all_classes_new.sh
@@ -1,8 +1,9 @@
 INPUT_FOLDER=$1
+LENGTH=$2
 
 ENR_DIR=`dirname $0`
 
 for f in `ls $INPUT_FOLDER`;
 do
-    python $ENR_DIR/replicate_last_frame_parallel.py $INPUT_FOLDER/$f/ 45
+    python $ENR_DIR/replicate_last_frame_parallel.py $INPUT_FOLDER/$f/ $LENGTH
 done
