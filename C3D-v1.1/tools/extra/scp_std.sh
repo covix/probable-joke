7927a2e1c6a8f957113ae661f0de76b8f45e8f68
Anto95 <tonio.db@hotmail.it>
2018-02-10 16:06:02 +0100

    modified plots

---
+++ C3D-v1.1/tools/extra/scp_std.sh
@@ -0,0 +1,5 @@
+scp -r apaladin@nef:/data/sparks/share/asl/probable-joke/C3D-v1.1/examples/asl_paper2/0_baseline_loud_loud/*5677491.stderr* .
+scp -r apaladin@nef:/data/sparks/share/asl/probable-joke/C3D-v1.1/examples/asl_paper2/0_baseline_replicated_replicated/*5650353.stderr* .
+scp -r apaladin@nef:/data/sparks/share/asl/probable-joke/C3D-v1.1/examples/asl_paper2/1_aligned-loud_aligned-loud/*5673366.stderr* .
+scp -r apaladin@nef:/data/sparks/share/asl/probable-joke/C3D-v1.1/examples/asl_paper2/1_aligned-original_aligned-original/*5677484.stderr* .
+
