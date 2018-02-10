d722a2f75194f246a0bda92833855d0e70283704
Luca Coviello <luca.coviello@gmail.com>
2018-02-10 14:35:16 +0100

    Added softmax-examination code for png

--- C3D-v1.1/examples/asl_paper2/TEST/softmax-examination/replicate_and_run.sh
+++ C3D-v1.1/examples/asl_paper2/TEST/softmax-examination/replicate_and_run.sh
@@ -1,13 +1,12 @@
 WEIGHTS=$1
+$CLASSES_TO_TEST="01 04 24 42 43"
 
-for i in `echo 01 04 24 42 43`;
+for i in `echo $CLASSES_TO_TEST`;
 do
     cp train_1_aligned_aligned.prototxt train_1_aligned_aligned_${i}.prototxt
-    sed -i.sed_bak "s/test_c04/test_c${i}/g" train_1_aligned_aligned_${i}.prototxt
-    sed -i.sed_bak "s/aligned_gctw_sample_test_reduced_fps_class\/04/aligned_gctw_sample_test_reduced_fps_class\/${i}/g" train_1_aligned_aligned_${i}.prototxt
+    sed -i.sed_bak "s/{CLASS}/${i}/g" train_1_aligned_aligned_${i}.prototxt
+    # sed -i.sed_bak "s/aligned_gctw_sample_test_reduced_fps_class\/04/aligned_gctw_sample_test_reduced_fps_class\/${i}/g" train_1_aligned_aligned_${i}.prototxt
     ./run_test_c3d_gpus.sh train_1_aligned_aligned_${i}.prototxt $WEIGHTS
 done
 
 rm *.sed_bak
-
-
