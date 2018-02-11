b89627afb530e2a573b63533af84beefd35c80df
schenock <mitrevdane@gmail.com>
2018-02-11 16:03:34 +0100

    ca marcheeeeeeeee

---
+++ C3D-v1.1/examples/asl_paper2/TEST/probability-extractor/run_all_original.sh
@@ -0,0 +1,11 @@
+MODEL=$1
+TEST_DATA_PATH="/data/sparks/share/asl/experiments/datasets/test/aligned_reduced_fps_original_test_all_classes/"
+
+echo "Testing started using model: " $MODEL
+
+for CLASS in {01..17}
+do
+echo "Testing for class: " $CLASS
+ ./replicate_and_run_class.sh $MODEL $TEST_DATA_PATH $CLASS
+done
+
